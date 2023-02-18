4e662894530accdfdc15e59f393cf2fe4ea4f917
oksanatok <121721023+oksanatok@users.noreply.github.com>
2023-02-18 20:51:03 +0100

    conversion units

--- src/app.js
+++ src/app.js
@@ -55,6 +55,7 @@ function showTemperature(response) {
   document.querySelector(".todayTemp").innerHTML = Math.round(
     response.data.main.temp
   );
+  celsiusTemp = response.data.main.temp;
   descriptionElement.innerHTML = response.data.weather[0].description;
   document.querySelector(".humidity").innerHTML = response.data.main.humidity;
   document.querySelector(".wind").innerHTML = Math.round(
@@ -92,10 +93,23 @@ function search(event) {
 // Convert units
 function showFahrTemp(event) {
   event.preventDefault();
-  let fahrTemp = (14 * 9) / 5 + 32;
+  let fahrTemp = (celsiusTemp * 9) / 5 + 32;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
 
   let temperatureElement = document.querySelector(".todayTemp");
   temperatureElement.innerHTML = Math.round(fahrTemp);
 }
+function showCelsiusTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(".todayTemp");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+}
+
+let celsiusTemp = null;
 let fahrenheitLink = document.querySelector("#fahrenheit");
 fahrenheitLink.addEventListener("click", showFahrTemp);
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", showCelsiusTemp);
